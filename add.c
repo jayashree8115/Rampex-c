ef32875aa2508cf333681e47fd158d58ff22d07d
jayashree8115 <jayashree08112005@gmail.com>
2025-05-12 15:52:34 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter a=");
+scanf("%d",&a);
+printf("enter b=");
+scanf("%d",&b);
+sum=a + b;
+printf("the sum a and b is %d ",sum);
+return 0;
+}
