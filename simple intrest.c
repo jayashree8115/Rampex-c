1e80bde54d8c7b2798e45d89ae5fb5d36ba5fa66
jayashree8115 <jayashree08112005@gmail.com>
2025-05-13 15:41:34 +0530

    Add files via upload

---
+++ simple intrest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int p,r,t,si;
+    printf("principle:");
+    scanf("%d",&p);
+    printf("rate:");
+    scanf("%d",&r);
+    printf("time:");
+    scanf("%d",&t);
+    si=p*r*t/100;
+    printf("simple intrest =%d",si);
+}
+    
