bce39534a14fd9cd01532d67fc96ec4c22a80caa
jayashree8115 <jayashree08112005@gmail.com>
2025-05-13 13:36:11 +0530

    Add files via upload

---
+++ name,age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ char name;
+ int age;
+ int salary;
+ printf(" enter the name:");
+ scanf("%c ",name);
+ printf("%c \n",name);
+ printf("\n enter the age:");
+ scanf("%d",age);
+ printf("\n enter salary:");
+ scanf("%d",salary);
+}
