173471cec94408c2931583ae27937a0d394b2fab
jayashree8115 <jayashree08112005@gmail.com>
2025-05-13 13:22:30 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,w,area,perimeter;
+    l=20;
+    w=3;
+    area=l*w;
+    printf("area of the reactangle: %d\n",area);
+    perimeter=l+w*2;
+    printf("perimeter of the rectangle: %d\n",perimeter);
+    
+}
