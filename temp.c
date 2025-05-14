3040d3f78f0fd9428bea61e98d0d7a289ab880ae
jayashree8115 <jayashree08112005@gmail.com>
2025-05-14 13:41:00 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int temp;
+    printf("enter temperature=");
+    scanf("%d",&temp);
+    if(temp<10)
+    {
+        printf("your outfit is jacket");
+    }
+    else if(temp<10 && temp>20)
+    {
+        printf("your outfit is sweater");
+    }
+    else
+    {
+        printf("your outfit is tshirt");
+    }
+    
+}
